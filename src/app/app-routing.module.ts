5948ede11b003a6bdd76fd730ee4fb7a2c84fa8a
Nicolas dos Santos Lima <lima.nicolas@fiergs.org.br>
2024-10-24 16:27:52 -0300

    ajuste rota embarque

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { LoginComponent } from './login/login.component';
 import { ViagensComponent } from './viagens/viagens.component';
 import { CadastrarViagemComponent} from './viagens/cadastrar-viagem/cadastrar-viagem.component';
 import { ClientesComponent } from './clientes/clientes.component';
-import { EmbarqueComponent } from './embarque/embarque.component';
 import { CadastrarEmbarqueComponent } from './embarque/cadastrar-embarque/cadastrar-embarque.component'
 import { EditarViagemComponent } from './viagens/editar-viagem/editar-viagem.component';
 import { CadastrarClienteComponent } from './clientes/cadastrar-cliente/cadastrar-cliente.component';
@@ -25,6 +24,7 @@ import { EditarNivelAcessoComponent } from './nivel-acesso/editar-nivel-acesso/e
 import { SobreViagemComponent } from './sobre-viagem/sobre-viagem.component';
 import { EditarEmbarqueDesembarqueComponent } from './embarque/editar-embarque-desembarque/editar-embarque-desembarque.component';
 import { ViagensLpComponent } from './viagens-lp/viagens-lp.component';
+import { EmbarqueComponent } from './embarque/embarque.component';
 
 const routes: Routes = [
 
@@ -67,6 +67,11 @@ const routes: Routes = [
   {
     path: 'usuarios',
     component: UsuariosComponent
+  },
+
+  {
+    path: 'embarque-desembarque',
+    component: EmbarqueComponent
   },
 
   {
@@ -167,11 +172,6 @@ const routes: Routes = [
   {
     path: 'religioso/:id',
     component: SobreViagemComponent
-  },
-
-  {
-    path: 'embarque-desembarque',
-    component: EmbarqueComponent
   },
 
   {
